ddc781192ecdfc123c29ef24eae0c6e0b0c5adba
gregoire charles <gcm.charles@gmail.com>
2016-11-24 12:50:27 +0100

    remove build

--- build/helpers/inline_svg.rb
+++
@@ -1,6 +0,0 @@
-def svg(name)
-  root = Middleman::Application.root
-  file_path = "#{root}/source/images/illustrations/misc/#{name}"
-  return File.read(file_path) if File.exists?(file_path)
-  '(not found)'
-end
--- build/javascripts/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.nav-links a').on('click', function() {
-		var section = $(this).attr('href');
-		var speed = 1000;
-		$('html, body').animate( { scrollTop: $(section).offset().top }, speed );
-		return false;
-	});
-});
