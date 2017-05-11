539b88c5564b07b930c9afbc040e77a0158d9798
gregoire charles <gcm.charles@gmail.com>
2017-05-11 13:13:59 +0200

    make confirmation more obvious when user submits contact form

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -10,11 +10,13 @@ $(document).ready(function(){
         success: function(data){
           $('#footer h3').html("Got it <i class='fa fa-check'></i>");
           $('#footer h3 i').css('color', '#4EFFBB');
+          $('#footer h3').addClass("fadeIn");
           $('.form').trigger("reset");
         },
         error: function(){
           $('#footer h3').html("Oops <i class='fa fa-times'></i>");
           $('#footer h3 i').css('color', '#ff4b47');
+          $('#footer h3').addClass("fadeIn");
           $('.form').trigger("reset");
         }
       });
